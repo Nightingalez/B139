4c1284eda636c72c4e8462ecddf48ef89ccd871b
Nightingalez <marc.haagensen@yahoo.dk>
2018-03-09 12:34:11 +0100

    Book update

---
+++ src/B139/Books.java
@@ -0,0 +1,20 @@
+package B139; import Users;
+
+public class Books {
+    private int id;
+    private int pageNumber;
+    private String title;
+    private String isbn;
+    private String auther;
+    private String publishingYear;
+    private String genre;
+    private User = renter;
+
+    public Book(){
+
+    }
+    public boolean isRented(){
+
+    }
+
+}
