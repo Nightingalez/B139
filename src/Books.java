f44a9644eba4d87b50943f4e52cfc45765b9768f
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2018-03-09 14:10:04 +0100

    Revert "Merge branch 'Mathias'"
    This reverts commit 29485b9f62801422446602786052289bc381cf35, reversing
    changes made to 24920a9de637692b6a001eaa3d3ee383150c7f7a.

---
+++ src/Books.java
@@ -0,0 +1,20 @@
+package B139; import User;
+
+public class Books {
+    private int id;
+    private int pageNumber;
+    private String title;
+    private String isbn;
+    private String auther;
+    private String publishingYear;
+    private String genre;
+    private User = renter;
+
+    public Book(){
+
+    }
+    public boolean isRented(){
+
+    }
+
+}
---
+++ src/Librarian.java
@@ -0,0 +1,19 @@
+package B139;
+
+public class Librarian extends Users{
+	
+	public Librarian() {
+	}
+	
+	public void addBook() {
+	}
+	
+	public void removeBook() {
+	}
+	
+	public void addMembership() {
+	}
+	
+	public void removeMembership() {
+	}
+}
